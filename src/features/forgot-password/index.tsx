e5d7fa7f8987170c9ca6c88f3e8e81a26d02e6a5
Abdurrohman Zorif <zorifimaduddin@gmail.com>
2024-12-09 15:35:37 +0700

    feat: forgot password (#5)

---
+++ src/features/forgot-password/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useFormik } from "formik";
+import { ForgotPasswordSchema } from "./schema";
+import useForgotPassword from "@/hooks/api/auth/useForgotPassword";
+
+const ForgotPasswordPage = () => {
+  const { mutateAsync: forgotPassword, isPending } = useForgotPassword();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: ForgotPasswordSchema,
+    onSubmit: async (values) => {
+      await forgotPassword(values);
+    },
+  });
+
+  return (
+    <main className="flex justify-center pt-20">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Forgot Password</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  placeholder="example@mail.com"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {!!formik.touched.email && !!formik.errors.email ? (
+                  <p className="text-xs text-red-500">{formik.errors.email}</p>
+                ) : null}
+              </div>
+            </div>
+
+            <Button type="submit" className="mt-4 w-full" disabled={isPending}>
+              {isPending ? "Loading..." : "Submit"}
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
+
+export default ForgotPasswordPage;
